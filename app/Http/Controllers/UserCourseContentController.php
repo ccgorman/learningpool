4053d6e2805f9e14a21fd1e0bc8d5f0fd0a31725
vagrant <vagrant@homestead>
2019-02-17 17:50:20 +0000

    Created models

---
+++ app/Http/Controllers/UserCourseContentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\UserCourseContent;
+use Illuminate\Http\Request;
+
+class UserCourseContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\UserCourseContent  $userCourseContent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserCourseContent $userCourseContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\UserCourseContent  $userCourseContent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserCourseContent $userCourseContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\UserCourseContent  $userCourseContent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserCourseContent $userCourseContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\UserCourseContent  $userCourseContent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserCourseContent $userCourseContent)
+    {
+        //
+    }
+}
---
+++ database/factories/CourseContentFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Model\CourseContent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
